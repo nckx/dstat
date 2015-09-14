5afc76b47259120be90e6009372bd67f810f9d38
Tobias Geerinckx-Rice <tobias.geerinckx.rice@gmail.com>
2015-09-14 20:58:10 +0200

    Fix (still hacky) fallback code in thermal plugin

--- plugins/dstat_thermal.py
+++ plugins/dstat_thermal.py
@@ -7,11 +7,10 @@ class dstat_plugin(dstat):
         self.width = 3
         self.scale = 20
 
-        if os.path.exists('/sys/bus/acpi/devices/LNXTHERM:01/thermal_zone/'):
-            self.vars = os.listdir('/sys/bus/acpi/devices/LNXTHERM:01/thermal_zone/')
-            self.nick = []
-            for name in self.vars:
-                self.nick.append(name.lower())
+        # This is extremely dirty and hard-coded, but is usually better than nothing
+        if os.path.exists('/sys/devices/virtual/thermal/thermal_zone0/'):
+            self.vars = ['temp', 'trip_point_0_temp']
+            self.nick = ['deg', 'lim']
 
         elif os.path.exists('/proc/acpi/ibm/thermal'):
             self.namelist = ['cpu', 'pci', 'hdd', 'cpu', 'ba0', 'unk', 'ba1', 'unk']
@@ -36,17 +35,17 @@ class dstat_plugin(dstat):
     def check(self):
         if not os.path.exists('/proc/acpi/ibm/thermal') and \
            not os.path.exists('/proc/acpi/thermal_zone/') and \
-           not os.path.exists('/sys/bus/acpi/devices/LNXTHERM:00/thermal_zone/'):
+           not os.path.exists('/sys/devices/virtual/thermal/thermal_zone0/'):
             raise Exception, 'Needs kernel ACPI or IBM-ACPI support'
 
     def extract(self):
-        if os.path.exists('/sys/bus/acpi/devices/LNXTHERM:01/thermal_zone/'):
+        if os.path.exists('/sys/devices/virtual/thermal/thermal_zone0/'):
             for zone in self.vars:
-                if os.path.isdir('/sys/bus/acpi/devices/LNXTHERM:01/thermal_zone/'+zone) == False:
-                    for line in dopen('/sys/bus/acpi/devices/LNXTHERM:01/thermal_zone/'+zone).readlines():
+                if os.path.isdir('/sys/devices/virtual/thermal/thermal_zone0/'+zone) == False:
+                    for line in dopen('/sys/devices/virtual/thermal/thermal_zone0/'+zone).readlines():
                         l = line.split()
                         if l[0].isdigit() == True:
-                            self.val[zone] = int(l[0])
+                            self.val[zone] = int(l[0]) / 1000
                         else:
                             self.val[zone] = 0
         elif os.path.exists('/proc/acpi/ibm/thermal'):
